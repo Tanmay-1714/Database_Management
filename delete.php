1c47c3380ce1c05ff4f8d80c10fa48c5b3c7fed5
tanmay tiwari <manjut866@gmail.com>
2024-12-14 11:33:05 +0530

    delete.php

---
+++ delete.php
@@ -0,0 +1,10 @@
+<?php
+include ('connect.php');
+$did=$_GET['delete_id'];
+$query="delete from crud where id=$did";
+$result=mysqli_query($con,$query);
+if($result){
+    echo"<script>alert ('Data deleted successfully')</script>";
+    echo"<script> window.open('display.php' , '_self') </script>" ;
+}
+?>
